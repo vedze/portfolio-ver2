06b52516ce528b0a0ad4974f4b4df9414a7e29c0
vedze <ezdev00@gmail.com>
2024-07-17 13:42:06 +0900

    Modify: Home Page 수정
    - 모바일 사이즈에서 width, margin 이상하게 남아 global css 및 page.tsx
      구조 다시 설정
    - Home Page css 수정
    - Home Page 미디어쿼리 추가

--- src/container/home/HomeContent.tsx
+++ src/container/home/HomeContent.tsx
@@ -9,9 +9,13 @@ export default function HomeContent() {
         이상을 꿈꾸는 개발자 <strong>이지은</strong>입니다.
       </div>
       <div className={styles.homeIntroduce}>
-        <div>여기는 제 소개가 들어갈 부분입니다</div>
-        <div>여기는 제 소개가 들어갈 부분입니다</div>
-        <div>여기는 제 소개가 들어갈 부분입니다</div>
+        <div>
+          여기는 제 소개가 들어갈 부분입니다.
+          <br />
+          줄글이 들어갑니다.
+          <br />
+          짧은 소개말 뭐라고 할 지 고민 백만번
+        </div>
       </div>
     </div>
   );
