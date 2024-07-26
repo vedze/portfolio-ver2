dc74d912cd41ff8779a2ee9328da32cfcbaf4e18
vedze <ezdev00@gmail.com>
2024-07-26 12:47:45 +0900

    Modify: project 데이터 및 페이지 구조 변경

--- src/container/projects/ProjectForm.tsx
+++ src/container/projects/ProjectForm.tsx
@@ -1,7 +1,6 @@
 import styles from "@/styles/projects/projectscontent.module.css";
 import { ProjectType } from "@/data/project";
 import { FiCheck } from "react-icons/fi";
-import Image from "next/image";
 
 interface ProjectProps {
   project: ProjectType;
@@ -12,44 +11,32 @@ export default function Project({ project }: ProjectProps) {
     <div className={styles.projectContainer}>
       <div className={styles.project}>
         <div className={styles.titleSection}>
-          <p>{project.type}</p>
           <h1>{project.title}</h1>
-          <time>{project.date}</time>
         </div>
         <div className={styles.summarySection}>
-          {project.summaries.map((summary, i) => (
+          <h2>Details</h2>
+          {project.details.map((detail, i) => (
             <p key={i}>
               <FiCheck style={{ marginRight: "10px" }} />
-              {summary}
+              {detail}
             </p>
           ))}
         </div>
         <div className={styles.skillSection}>
-          <h2>사용 기술</h2>
+          <h2>Skills</h2>
           {project.skills.map((skill, idx) => (
             <p key={idx}>{skill}</p>
           ))}
         </div>
         {project.links && project.links.length > 0 && (
           <div className={styles.linkSection}>
-            <h2>링크</h2>
+            <h2>Link</h2>
             {project.links.map((link, idx) => (
               <p key={idx}>{link}</p>
             ))}
           </div>
         )}
       </div>
-      {project.imgs && project.imgs.length > 0 && (
-        <div className={styles.projectImg}>
-          <Image
-            src={project.imgs[0]}
-            alt={project.title}
-            width={500}
-            height={300}
-            layout="responsive"
-          />
-        </div>
-      )}
     </div>
   );
 }
--- src/data/project.ts
+++ src/data/project.ts
@@ -1,34 +1,83 @@
 export interface ProjectType {
-  type: string;
   title: string;
-  date: string;
-  summaries: string[];
+  status?: string;
+  about: {
+    type: string;
+    date?: string;
+    member?: string;
+  };
   skills: string[];
+  details: string[];
   links?: string[];
-  imgs?: string[];
 }
 
 export const projects: ProjectType[] = [
   {
-    type: "개인 프로젝트",
-    title: "개인 포트폴리오 사이트",
-    date: "2024.06.25 - 진행중",
-    summaries: [
-      "개인 포트폴리오 제작 프로젝트입니다.",
-      "포트폴리오 summary 1",
-      "포트폴리오 summary 2",
+    title: "개인 포트폴리오",
+    status: "(현재 사이트)",
+    about: {
+      type: "개인 프로젝트",
+      date: "2024.06 - 진행중",
+    },
+    skills: ["React", "TypeScript", "NextJS", "Module CSS"],
+    details: [
+      "나만의 포트폴리오를 갖기 위해 제작",
+      "nodemailer를 이용한 메일링 기능",
+      "clipboard API를 이용한 복사 기능",
     ],
-    skills: ["React", "TypeScript", "NextJS"],
-    links: ["github link"],
-    imgs: ["/images/projects/portfolio-1.jpg"],
+    links: ["Github Link"],
   },
   {
-    type: "팀 프로젝트",
-    title: "Team Portfolio",
-    date: "2024.03.20 - 2024.04.18",
-    summaries: ["누구나 이용할 수 있는 공유 포트폴리오 사이트 프로젝트입니다."],
-    skills: ["React", "TypeScript"],
-    // links: ["https://github.com/Plain-Portfolio/portfolio_front"],
-    imgs: ["/images/projects/team-portfolio-1.jpg"],
+    title: "감정 일기장",
+    about: {
+      type: "개인 프로젝트",
+      date: "2024.05",
+    },
+    skills: ["React", "JavaScript"],
+    details: ["감정과 함께 기록할 수 있는 일기장 프로젝트"],
+    links: ["Github Link"],
   },
+  {
+    title: "TEAM PORTFOLIO",
+    status: "(배포 중단)",
+    about: {
+      type: "팀 프로젝트",
+      date: "2024.03 - 2024.04",
+      member: "프론트엔드 3명, 백엔드 1명",
+    },
+    skills: [
+      "React",
+      "TypeScript",
+      "Styled-components",
+      "Swagger API",
+      "POSTMAN",
+    ],
+    details: [
+      "누구나 열람하고 게시할 수 있는 포트폴리오 사이트",
+      "중도 참여하여 유저별 프로젝트 페이지 제작",
+    ],
+    links: ["Github Link"],
+  },
+  // {
+  //   type: "개인 프로젝트",
+  //   title: "개인 포트폴리오 사이트",
+  //   date: "2024.06.25 - 진행중",
+  //   summaries: [
+  //     "개인 포트폴리오 제작 프로젝트입니다.",
+  //     "포트폴리오 summary 1",
+  //     "포트폴리오 summary 2",
+  //   ],
+  //   skills: ["React", "TypeScript", "NextJS"],
+  //   links: ["github link"],
+  //   imgs: ["/images/projects/portfolio-1.jpg"],
+  // },
+  // {
+  //   type: "팀 프로젝트",
+  //   title: "Team Portfolio",
+  //   date: "2024.03.20 - 2024.04.18",
+  //   summaries: ["누구나 이용할 수 있는 공유 포트폴리오 사이트 프로젝트입니다."],
+  //   skills: ["React", "TypeScript"],
+  //   // links: ["https://github.com/Plain-Portfolio/portfolio_front"],
+  //   imgs: ["/images/projects/team-portfolio-1.jpg"],
+  // },
 ];
