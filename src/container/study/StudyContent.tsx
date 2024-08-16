def4015f8fcd60fa2bde8cc13975a3965ee40bc5
vedze <ezdev00@gmail.com>
2024-08-16 15:57:56 +0900

    Design: Study page 디자인 세팅

--- src/container/study/StudyContent.tsx
+++ src/container/study/StudyContent.tsx
@@ -1,20 +1,19 @@
+import styles from "@/styles/study/studycontent.module.css";
 import { studyData } from "@/data/study";
+import { IoMdArrowDropright } from "react-icons/io";
 
 export default function StudyContent() {
   return (
     <div>
-      <p>
-        관심 분야가 넓어 이것저것 다 공부하고 있어요.
-        <br />
-        의미없는 시간은 없다고 생각하기에, 이 모든 것들은 결국 제 강한 무기가 될
-        거라고 믿어요.
-      </p>
       {studyData.map((study, i) => (
-        <div key={i}>
+        <div key={i} className={styles.studyForm}>
           <h1>{study.category}</h1>
           {study.lists.map((list, i) => (
-            <div key={i}>
-              <strong>{list.name}</strong>
+            <div key={i} className={styles.list}>
+              <h4>
+                <IoMdArrowDropright />
+                {list.name}
+              </h4>
               <p>{list.expl}</p>
             </div>
           ))}
