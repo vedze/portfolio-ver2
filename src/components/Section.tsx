886762b4fcc37b5f8e3c6fc0d4836ded7cc3da4f
vedze <ezdev00@gmail.com>
2024-08-16 15:59:06 +0900

    Modify: Section intro 추가
    - 각 섹션 머리글을 분리하여 공통 컴포넌트 Section에서 렌더링하도록 함

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,13 +3,17 @@ import styles from "@/styles/components/section.module.css";
 interface ISection {
   num: string;
   name: string;
+  intro?: string;
 }
 
-export default function Section({ num, name }: ISection) {
+export default function Section({ num, name, intro }: ISection) {
   return (
-    <div className={styles.section}>
-      <span>{num}</span>
-      <span>{name}</span>
+    <div>
+      <div className={styles.section}>
+        <span>{num}</span>
+        <span>{name}</span>
+      </div>
+      {intro && <p className={styles.intro}>{intro}</p>}
     </div>
   );
 }
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -9,3 +9,9 @@ export const personalCons = {
 };
 
 export const CATCH_PHRASE = "가치를 쌓아가는 개발자";
+
+export const INTRO = {
+  studyIntro:
+    "관심 분야가 넓어 이것저것 다 공부해보고 있어요.\n" +
+    "의미없는 시간은 없다고 생각하기에, 이 모든 것들은 결국 제 강한 무기가 될 거라고 믿어요.",
+};
